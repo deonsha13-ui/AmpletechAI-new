221168ed7e13853d1080280d099798ba7fae8618
deonsha13-ui <deonsha13@gmail.com>
2026-07-01 01:50:33 +0530

    Text colour

--- test-img.js
+++ test-img.js
@@ -1,26 +1,14 @@
 import fs from 'fs';
 import * as cheerio from 'cheerio';
 
-const html = fs.readFileSync('case-studies.html', 'utf8');
-const $ = cheerio.load(html, { _useHtmlParser2: true, decodeEntities: false });
+const html = fs.readFileSync('index.html', 'utf8');
+const $ = cheerio.load(html);
 
-const leadFollowUpLinks = $('a:contains("Lead Follow-Up Automation")');
-console.log('Lead Follow-Up Automation links:', leadFollowUpLinks.length);
-leadFollowUpLinks.each((i, el) => {
-    const img = $(el).find('img');
-    console.log('Current src:', img.attr('src'));
-    img.attr('src', '/assets/case-study-realestate.png');
-    img.attr('srcset', '/assets/case-study-realestate.png 512w,/assets/case-study-realestate.png 1024w,/assets/case-study-realestate.png 1200w');
+// Find all script tags
+const scripts = $('script');
+console.log(`Found ${scripts.length} script tags in index.html`);
+
+scripts.each((i, script) => {
+  const $s = $(script);
+  console.log(`Script ${i + 1}: src=${$s.attr('src')} / type=${$s.attr('type')} / content=${$s.text().substring(0, 300)}`);
 });
-
-const supportMvpLinks = $('a:contains("AI Support Assistant MVP")');
-console.log('AI Support Assistant MVP links:', supportMvpLinks.length);
-supportMvpLinks.each((i, el) => {
-    const img = $(el).find('img');
-    console.log('Current src:', img.attr('src'));
-    img.attr('src', '/assets/case-study-ecommerce.png');
-    img.attr('srcset', '/assets/case-study-ecommerce.png 512w,/assets/case-study-ecommerce.png 1024w,/assets/case-study-ecommerce.png 1200w');
-});
-
-fs.writeFileSync('case-studies-modified-2.html', $.html());
-console.log('Saved to case-studies-modified-2.html');
